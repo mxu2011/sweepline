8ab3e8d40f0b0e4c5e1b8968c01557e9dd5ffa0b
Martin Xu <mxu2008@gmail.com>
2013-05-06 01:20:20 -0700

    made test_sweep_line.js working

--- test/test_sweep_line.js
+++ test/test_sweep_line.js
@@ -6,10 +6,10 @@ var sl = require('./index')
   , assert = require('assert')
   , _ = require('underscore');
 
-  exports['x'] = function(){}
 
 
-exports['test can find'] = function(){
+
+
   geojson = [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]];
   points  = _.map(geojson, function(pnt){ return new Point(pnt[0],pnt[1]); });
   polygon = new Polygon(points);
@@ -20,8 +20,10 @@ exports['test can find'] = function(){
     sweep_line.add(ev);
   }
   
-  assert.ok(sweep_line.find({edge:1}));    
-};
+  var nd = sweep_line.find({edge:1});
+
+  console.log("nd="+nd.toString())
+
 
 
 //more tests here!
